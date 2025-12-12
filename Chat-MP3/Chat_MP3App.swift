68d96b270e15cc2d6544d9220ae7b2391a6d4516
Liam Ronan <Liampronan@gmail.com>
2025-12-12 15:08:14 -0800

    Initial Commit

---
+++ Chat-MP3/Chat_MP3App.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_MP3App.swift
+//  Chat-MP3
+//
+//  Created by Liam Ronan on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Chat_MP3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
